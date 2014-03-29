248736f95c546401ed375d1b0c51b55808049ffa
Carlos Giraldo <cacu2ggg@gmail.com>
2014-03-29 21:42:10 +0100

    cronos

--- src/Chronos.java
+++ src/Chronos.java
@@ -1,4 +1,32 @@
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class Chronos {
+
+public class Chronos extends Thread{
+	
+	private long period;  // segundos
+	private static Timer tiempo = null;
+	private static Executor ejecutor;
+	
+	public Chronos(long p){
+		this.period = p;
+		ejecutor = new Executor();
+	}
+	
+	private class Task extends TimerTask{
+		public void run(){
+			System.out.println("sunea alarma");
+			ponerAlarma();
+		}
+	}
+	
+	public void ponerAlarma(){
+		if(tiempo == null) {
+			tiempo = new Timer();
+		}
+		tiempo.schedule(new Task(), period*1000);
+	}
+
+
 
 }
--- src/Drunken.java
+++ src/Drunken.java
@@ -2,6 +2,10 @@
 public class Drunken {
 
 	static Chronos chrono;
-	static Executor executor;
 	
+	public static void main(String[] args){
+		chrono = new Chronos(2);
+		chrono.ponerAlarma();
+		
+	}
 }
