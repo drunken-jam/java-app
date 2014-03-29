fa99c4b10992a7812e3f537cf858dbb6e885266e
manutero <manutero@gmail.com>
2014-03-29 22:17:07 +0100

    chronos y Executor

--- src/Chronos.java
+++ src/Chronos.java
@@ -1,30 +1,35 @@
 import java.util.Timer;
 import java.util.TimerTask;
+import model.Game;
 
 
-public class Chronos extends Thread{
-	
+public class Chronos {
+
 	private long period;  // segundos
-	private static Timer tiempo = null;
-	private static Executor ejecutor;
-	
+	private static Timer timer = null;
+	private Executor ejecutor;
+
 	public Chronos(long p){
 		this.period = p;
 		ejecutor = new Executor();
 	}
-	
+
 	private class Task extends TimerTask{
+		@Override
 		public void run(){
 			System.out.println("sunea alarma");
+			Game game = ejecutor.giveMeGame();
+			System.out.println(game.getAudio());
+			timer.cancel();
 			ponerAlarma();
 		}
 	}
-	
+
 	public void ponerAlarma(){
-		if(tiempo == null) {
-			tiempo = new Timer();
-		}
-		tiempo.schedule(new Task(), period*1000);
+
+		timer = new Timer();
+
+		timer.schedule(new Task(), period*1000);
 	}
 
 
--- src/Executor.java
+++ src/Executor.java
@@ -1,6 +1,31 @@
+import java.util.HashMap;
+import java.util.Random;
+import model.Game;
+
 
 public class Executor {
 
-	
-	
+	private static final HashMap<Integer, String> gameIndex;
+	static {
+		gameIndex = new HashMap<Integer, String>();
+		gameIndex.put(1, "one");
+		gameIndex.put(2, "two");
+	}
+
+	public Game giveMeGame() {
+		String audio = gameIndex.get(nextGame());
+		return new Game(audio);
+	}
+
+	private static Integer nextGame() {
+		return nextGame(1, 2);
+	}
+
+	// stackoverflow: 363681
+	private static Integer nextGame(int min, int max) {
+		Random rand = new Random();
+		int randomNum = rand.nextInt((max - min) + 1) + min;
+		return randomNum;
+	}
+
 }
